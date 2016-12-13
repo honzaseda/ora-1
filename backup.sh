a8280bb7180baef0d9d9f5e6b8959a4826369911
Filip Jani <jsem@filek.cz>
2016-12-13 18:40:57 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /home/oracle/ora_backup
+
+echo "Zalohuji control01.ctl"
+cp /u01/app/oracle/oradata/orcl12c/control01.ctl /home/oracle/ora_backup/control01.ctl
+
+echo "Zalohuji redo logy"
+cp /u01/app/oracle/oradata/orcl12c/redo01.log /home/oracle/ora_backup/redo01.log
+cp /u01/app/oracle/oradata/orcl12c/redo02.log /home/oracle/ora_backup/redo02.log
+cp /u01/app/oracle/oradata/orcl12c/redo03.log /home/oracle/ora_backup/redo03.log
+
+echo "Zalohuji spfile"
+cp /u01/app/oracle/product/12.1.0.2/db_1/dbs/spfileorcl12c.ora /home/oracle/ora_backup/spfileorcl12c.ora
+
+./save_tree1.sh
