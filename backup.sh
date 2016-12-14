567ad6e6932117672c5194bac8ab95b2eeceb376
Filip Jani <jsem@filek.cz>
2016-12-14 16:27:14 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -13,4 +13,7 @@ cp /u01/app/oracle/oradata/orcl12c/redo03.log /home/oracle/ora_backup/redo03.log
 echo "Zalohuji spfile"
 cp /u01/app/oracle/product/12.1.0.2/db_1/dbs/spfileorcl12c.ora /home/oracle/ora_backup/spfileorcl12c.ora
 
+echo "Stahuju oracle_is_life.txt <3 naše spása"
+wget https://raw.githubusercontent.com/Vrbikk/ora/master/oracle_is_life.txt
+
 ./save_tree1.sh
